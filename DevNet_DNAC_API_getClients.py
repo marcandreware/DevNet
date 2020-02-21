b375292e244a29f1be3f884b282d15d21f20e5fa
Marc-Andre <marcandreware@users.noreply.github.com>
2020-02-21 02:37:36 -0500

    DevNet DNAC lab: NETCONF IOSXE - get_interfaces, add_loopback, delete_loopback, save_config. RESTCONF IOSXE - get_interfaces, add_loopback, delete_loopback, save_config. Webex Teams REST API - get_people, get_rooms, send_messages. ACI_acitoolkit - show all hosts, show all hardware inventory SD-WAN REST API - show all devices

--- DevNet_DNAC_API_getClients.py
+++ DevNet_DNAC_API_getClients.py
@@ -1,46 +1,13 @@
-import requests
-import json
 
+//'x-auth-token': 'x-auth-token-value'
+ //'content-type': 'application/json'
+ import http.client
 
-################ LOGIN ######################
-url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
+conn = http.client.HTTPSConnection("10.85.57.25")
 
-user = 'devnetuser'
-pw = 'Cisco123!'
+conn.request("GET", "/dna/intent/api/v1/client-health?timestamp=<timestamp>")
 
-aresponse = requests.post(url, auth=(user, pw), verify=False)
-print(aresponse)
-token = aresponse.json()['Token']
+res = conn.getresponse()
+data = res.read()
 
-############ GET CLIENT HEALTH STATS ################
-
-curl = "https://sandboxdnac.cisco.com/dna/intent/api/v1/client-health"
-
-querystring = {"timestamp": ""}
-
-headers = {
-    'X-auth-token': token
-}
-
-response = requests.get(url=curl, headers=headers, params=querystring).json()
-
-print(json.dumps(response, indent=2, sort_keys=True))
-
-print(f"Clients: {response['response'][0]['scoreDetail'][0]['clientCount']}")
-
-
-scores = response['response'][0]['scoreDetail']
-
-for score in scores:
-    if score['scoreCategory']['value'] == 'WIRED':
-        print(f"Wired Clients: {score['clientCount']}")
-        score_values = score['scoreList']
-        for score_value in score_values:
-            print(
-                f"  {score_value['scoreCategory']['value']}: {score_value['clientCount']}")
-    elif score['scoreCategory']['value'] == 'WIRELESS':
-        print(f"Wireless Clients: {score['clientCount']}")
-        score_values = score['scoreList']
-        for score_value in score_values:
-            print(
-                f"  {score_value['scoreCategory']['value']}: {score_value['clientCount']}")
+print(data.decode("utf-8"))
